fa1226c06ec9b4522c77a79ce204ea867565a5c2
John Salvador <john_salvador@ctl.creative.com>
2020-02-17 16:38:26 +0800

    Initial Commit

---
+++ Cocoacast/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cocoacast
+//
+//  Created by John Lawrence Salvador on 2/17/20.
+//  Copyright © 2020 John Lawrence Salvador. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
