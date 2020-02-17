fe5ac0f7c94cc9b4a136389482522780aa9b2ee2
John Salvador <john_salvador@ctl.creative.com>
2020-02-17 17:03:07 +0800

    Project Setup

---
+++ Cocoacast/Application Delegate/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  Cocoacast
+//
+//  Created by John Lawrence Salvador on 2/17/20.
+//  Copyright © 2020 John Lawrence Salvador. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
---
+++ Podfile
@@ -0,0 +1,23 @@
+target 'Cocoacast' do
+  platform :ios, '12.0'
+  use_frameworks!
+
+  # Pods for Cocoacast
+
+  # Wrappers
+  pod 'KeychainAccess'
+  pod 'ReachabilitySwift'
+
+  # Development
+  pod 'Reveal-SDK', configurations: ['Debug']
+
+  target 'CocoacastTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'CocoacastUITests' do
+    # Pods for testing
+  end
+
+end
